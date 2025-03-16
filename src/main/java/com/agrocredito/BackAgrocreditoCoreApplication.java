a91d2520a7ee2b3d0e067c378f4ca397a88e0089
monica <rojascelis10@gmail.com>
2025-03-16 12:33:50 -0500

    creacion del proyecto

---
+++ src/main/java/com/agrocredito/BackAgrocreditoCoreApplication.java
@@ -0,0 +1,13 @@
+package com.agrocredito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackAgrocreditoCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackAgrocreditoCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agrocredito/BackAgrocreditoCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agrocredito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackAgrocreditoCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
